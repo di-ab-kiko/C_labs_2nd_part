3dd774106342d2ed4ea6a656daf9d4750b34e9e9
di-ab-kiko <55152665+di-ab-kiko@users.noreply.github.com>
2020-07-01 14:10:41 +0300

    Update main.c

--- Clab_2nd_part/Монте-Карло/main.c
+++ Clab_2nd_part/Монте-Карло/main.c
@@ -5,30 +5,11 @@
 #include <locale.h>
 
 
-#define MAX_ITER 100
+#define N 100
+#define M_PI 3.14159265358979323846
 
 int main() {
-//    long long int i, n2 = 0;
-//    long double a, b;
-//    long double Pi;
-//    time_t t;
-//
-//    setlocale(LC_NUMERIC, "");
-//    struct lconv *ptrLocale = localeconv();
-//    ptrLocale->decimal_point = ",";
-//
-//    srand((unsigned) time(&t));
-//
-//    for (i = 0; i < MAX_ITER; i++) {
-//        a = (double)rand() / (double)RAND_MAX;
-//        b = (double)rand() / (double)RAND_MAX;
-//        n2 = (sqrt(pow(a, 2) + pow(b, 2)) <= 1.0f) ? ++n2 : n2;
-//
-//        if (i % 10000000 == 0) {
-//            Pi = (double)n2 / i * 4;
-//            printf("%1.20llf\n", Pi);
-//        }
-//    }
+    
 
     double x, y;
     double PI = 0;
@@ -36,12 +17,12 @@ int main() {
     int count = 0;
     char enter = 0;
 
-    for (int i = 1; i < MAX_ITER; i += 2)
+    for (int i = 1; i < N; i += 2)
     {
         PI += z * 4 / i;
         z *= -1;
 
-        printf("%f\n", PI);
+        printf("%f\n", M_PI-PI);
     }
     printf("Для выхода из программы нажмите enter ");
     scanf("%c\n",enter);
