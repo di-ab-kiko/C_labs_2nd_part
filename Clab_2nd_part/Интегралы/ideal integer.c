9c65662cb3dd34d51c08883d749ac3f58caac6fc
di-ab-kiko <Kiko0115217@gmail.com>
2020-09-16 21:09:14 +0300

    Update ideal integer.c

--- Clab_2nd_part/Интегралы/ideal integer.c
+++ Clab_2nd_part/Интегралы/ideal integer.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
 
-#define N 1000000
+#define  MAX_ITER 3 // максимальное число интервал
+#define ideal_value 2.3333333
+double f(double); // исходная функция
+double rectIntegral(double x1,double start_x, int N); // метод прямоугольников
 
-double F (double x)
+int main()
 {
-    double f;
-    f = tan(10*pow(x,2) + 29*x + 18);
-    return f;
-}
-
-int main ()
-{
-    double S = 0, x, a, b, h;
     const double Pi = 3.14159;
-    a = 0;
-    b = 2*Pi;
-    //отрезок [a, b] разобьем на N частей
-    h = (b - a)/N;
-    x = a + h;
-    while (x < b)
-    {
-        S = S + 4*F(x);
-        x = x + h;
-        //проверяем не вышло ли значение x за пределы полуинтервала [a, b)
-        if (x >= b) break;
-        S = S + 2*F(x);
-        x = x + h;
-    }
-    S = (h/3)*(S + F(a) + F(b));
-    printf ("%f", S);
+    printf("Рассчитываем значение интеграла для a=1 на отрезке [0;2Pi]\n");
+    printf("Интегрирование методом прямоугольников = %f\n", rectIntegral( 1,2, MAX_ITER));
+    getchar();
     return 0;
 }
+
+double f( double x) { // исходная функция
+    return pow(x,2);
+}
+double rectIntegral( double x1,double start_x, int N)
+{ // метод прямоугольников
+    a = 0.0;
+    double h = (x1 - start_x) / N, I = 0;
+    x1 = a + h;
+    for (I=1, I<= N-1, I++)
+            I += f(a, x1 + h / 2)
+    return h*I;
